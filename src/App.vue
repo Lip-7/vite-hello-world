0b9fa1d7a0fdb3ced2c5ec638fb3caa0226a4740
Lip-7 <zhighofficial@gmail.com>
2023-04-12 15:41:37 +0200

    minor fixes

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <AppTitle/>
+  </header>
+</template>
+
+<script>
+import AppTitle from "./components/AppTitle.vue";
+export default {
+  components: {
+    AppTitle,
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="title-wrapper">
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppTitle',
+    data() {
+        return {
+            title: 'Hello World',
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .title-wrapper{
+        display: flex;
+        justify-content: center;
+        padding: 20px 0;
+    }
+</style>
